ade8a433974cb52fedb0341f71401ba3ddb6e616
Valrhoman <valrhomanferrer@gmail.com>
2025-04-22 14:18:03 +0800

    enhance: hero responsiveness

--- components/Hero/ImageSlider/Dots.tsx
+++ components/Hero/ImageSlider/Dots.tsx
@@ -1,18 +1,18 @@
 export default function Dots({ onClick, visibleEl }: ImageSliderProps) {
   return (
-    <div className="absolute bottom-0 right-0 left-0 flex gap-4 justify-center mb-8">
+    <>
       {Array.from({ length: 2 }, (_, i: number) => {
         return (
           <div
             id={i.toString()}
             key={i}
             onClick={onClick}
-            className={`w-2 h-2 ${
-              visibleEl === i ? "bg-gray-600" : "bg-gray-300 cursor-pointer"
-            } rounded-md`}
+            className={`w-12 h-2 ${
+              visibleEl === i ? "bg-gray-500" : "bg-gray-200 cursor-pointer"
+            } rounded-md transition-all duration-300`}
           ></div>
         );
       })}
-    </div>
+    </>
   );
 }
--- components/Nav/NavBar.tsx
+++ components/Nav/NavBar.tsx
@@ -7,28 +7,14 @@ import { useSession } from "next-auth/react";
 import { useEffect, useRef, useState } from "react";
 import SearchBar from "../SearchBar";
 import NavPanel from "./NavPanel";
+import clsx from "clsx";
 
 export const navLinks = [
-  {
-    name: "Home",
-    href: "/",
-  },
-  {
-    name: "Shop",
-    href: "/",
-  },
-  {
-    name: "How It Works",
-    href: "/",
-  },
-  {
-    name: "Blog",
-    href: "/",
-  },
-  {
-    name: "Contact",
-    href: "/",
-  },
+  { name: "Home", href: "/" },
+  { name: "Shop", href: "/" },
+  { name: "How It Works", href: "/" },
+  { name: "Blog", href: "/" },
+  { name: "Contact", href: "/" },
 ];
 
 export default function NavBar() {
@@ -37,10 +23,24 @@ export default function NavBar() {
   });
 
   const [showSearchBar, setShowSearchBar] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const inputRef2 = useRef<HTMLInputElement>(null);
   const [showPanel, setShowPanel] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
+
+  const desktopInputRef = useRef<HTMLInputElement>(null);
+  const mobileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (showSearchBar) {
+      desktopInputRef.current?.focus();
+      mobileInputRef.current?.focus();
+    }
+  }, [showSearchBar]);
+
+  useEffect(() => {
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   function handleSearchButtonClick() {
     setShowSearchBar(!showSearchBar);
@@ -54,31 +54,13 @@ export default function NavBar() {
     setShowPanel(!showPanel);
   }
 
-  useEffect(() => {
-    if (showSearchBar) {
-      inputRef.current?.focus();
-      inputRef2.current?.focus();
-    }
-  }, [showSearchBar]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 0);
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <>
       <div
-        className={`fixed top-0 z-50 w-full py-6 px-8 md:px-16 md:py-10 lg:py-12  border-slate-100 bg-white ${
+        className={`fixed top-0 z-50 w-full px-8 py-6 sm:px-12 sm:py-8 md:px-16 md:py-10 lg:py-12  border-slate-100 bg-white ${
           isScrolled ? "shadow-md" : ""
-        }`}
+        } `}
       >
-        {/* <div className="max-w-7xl mx-auto"> */}
         <div className="flex items-center justify-between h-10 lg:max-w-[120rem] mx-auto ">
           <Link href="/">
             <div className="w-36 h-10 relative">
@@ -101,9 +83,12 @@ export default function NavBar() {
                     <Link
                       key={link.name}
                       href={link.href}
-                      className={`text-gray-600 hover:text-greeny-400 text-2xl ${
-                        link.name === "Home" ? "text-greeny-400" : ""
-                      }`}
+                      className={clsx(
+                        "text-2xl hover:text-greeny-400",
+                        link.name === "Home"
+                          ? "text-greeny-400"
+                          : "text-gray-600"
+                      )}
                     >
                       {link.name}
                     </Link>
@@ -113,7 +98,7 @@ export default function NavBar() {
 
               {showSearchBar && (
                 <div className="items-center justify-center gap-4 hidden md:flex">
-                  <SearchBar className={`w-full`} inputRef={inputRef2} />
+                  <SearchBar className={`w-full`} inputRef={desktopInputRef} />
                   <button
                     type="button"
                     className="self-center ml-2 text-gray-400 hover:text-greeny-400"
@@ -154,12 +139,17 @@ export default function NavBar() {
             </Link>
           </div>
         </div>
+
+        {/* Mobile Search */}
         <div
           className={`flex items-center justify-center gap-4 w-full mt-8 md:hidden ${
             showSearchBar ? "block" : "hidden"
           } `}
         >
-          <SearchBar className="w-full md:w-1/2 lg:w-1/3" inputRef={inputRef} />
+          <SearchBar
+            className="w-full md:w-1/2 lg:w-1/3"
+            inputRef={mobileInputRef}
+          />
           <button
             type="button"
             className="self-center ml-3 text-gray-400 hover:text-greeny-400"
@@ -170,25 +160,10 @@ export default function NavBar() {
         </div>
       </div>
 
-      <div className=" w-full py-6 px-8 md:px-16 md:py-8 ">
+      {/* Spacer for fixed navbar */}
+      <div className=" w-full px-8 py-6 sm:px-12 sm:py-8 md:px-16 md:py-10 lg:py-12">
         <div className="h-10"></div>
       </div>
-
-      <div className="h-14 md:h-18 gap-8 items-center justify-center hidden md:flex">
-        {/* {navLinks.map((link) => (
-          <Link
-            key={link.name}
-            href={link.href}
-            className="text-gray-500 hover:text-greeny-500 text-2xl font-semibold"
-          >
-            {link.name}
-          </Link>
-        ))} */}
-      </div>
-
-      {/* <div className="fixed top-0 w-full py-6 px-8 bg-red-700 opacity-25">
-        asdf
-      </div> */}
 
       <NavPanel open={showPanel} setOpen={setShowPanel} />
 
