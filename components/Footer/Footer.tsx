f51ef7371ac8e958fcbba573fef79cc542f7f8eb
Valrhoman <valrhomanferrer@gmail.com>
2025-10-01 11:50:45 +0800

    fix login responsiveness, and enhance footer font-sizing, and css nesting error

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,57 +1,57 @@
-import Link from "next/link";
-import Image from "next/image";
-import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
-import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
+import Link from 'next/link';
+import Image from 'next/image';
+import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs';
+import { HiOutlinePhone, HiOutlineMail } from 'react-icons/hi';
 export default function Footer() {
   return (
-    <div className="border-t-2 border-gray-100">
-      <div className="px-8 font-poppins py-8 md:py-16 transition-all sm:px-16 w-full max-w-[120rem] sm:mx-auto">
-        <div className="grid grid-cols-2 gap-y-8 sm:grid-cols-3 mb-8 md:mb-16">
+    <div className='border-t-2 border-gray-100'>
+      <div className='px-8 font-poppins py-8 md:py-16 transition-all sm:px-16 w-full max-w-[120rem] sm:mx-auto'>
+        <div className='grid grid-cols-2 gap-y-8 sm:grid-cols-3 mb-8 md:mb-16'>
           <div>
-            <h3 className="footer-header">Get to know us</h3>
-            <ul className="footer-list">
-              <li className="footer-li">Company</li>
-              <li className="footer-li">About</li>
-              <li className="footer-li">Blog</li>
-              <li className="footer-li">Help Center</li>
-              <li className="footer-li">Our Value</li>
+            <h3 className='footer-header'>Get to know us</h3>
+            <ul className='footer-list'>
+              <li className='footer-li'>Company</li>
+              <li className='footer-li'>About</li>
+              <li className='footer-li'>Blog</li>
+              <li className='footer-li'>Help Center</li>
+              <li className='footer-li'>Our Value</li>
             </ul>
           </div>
           <div>
-            <h3 className="footer-header">Be Our Partner</h3>
-            <ul className="footer-list">
-              <li className="footer-li">Local Farms</li>
-              <li className="footer-li">Reseller</li>
-              <li className="footer-li">Local Markets</li>
-              <li className="footer-li">Content Creators</li>
-              <li className="footer-li">Press</li>
+            <h3 className='footer-header'>Be Our Partner</h3>
+            <ul className='footer-list'>
+              <li className='footer-li'>Local Farms</li>
+              <li className='footer-li'>Reseller</li>
+              <li className='footer-li'>Local Markets</li>
+              <li className='footer-li'>Content Creators</li>
+              <li className='footer-li'>Press</li>
             </ul>
           </div>
-          <div className="sm:items-start col-span-2 sm:col-span-1">
-            <h3 className="footer-header">Payment Partners</h3>
-            <ul className="flex gap-4 w-48 sm:w-56 md:w-64 lg:w-80 h-16 sm:h-20 md:h-24 lg:h- mt-4 sm:mt-6 md:mt-8">
-              <div className="relative w-1/3">
+          <div className='sm:items-start col-span-2 sm:col-span-1'>
+            <h3 className='footer-header'>Payment Partners</h3>
+            <ul className='flex gap-4 w-48 sm:w-48 md:w-56 lg:w-64 h-16 sm:h-20 md:h-24 lg:h- mt-4 sm:mt-6 md:mt-8'>
+              <div className='relative w-1/3'>
                 <Image
-                  src="/logos/paypal.svg"
-                  alt="paypal"
-                  loading="lazy"
+                  src='/logos/paypal.svg'
+                  alt='paypal'
+                  loading='lazy'
                   fill
                 />
               </div>
-              <div className="relative w-1/3">
+              <div className='relative w-1/3'>
                 <Image
-                  src="/logos/Mastercard_Logo_1996.svg"
-                  alt="paypal"
-                  loading="lazy"
+                  src='/logos/Mastercard_Logo_1996.svg'
+                  alt='paypal'
+                  loading='lazy'
                   fill
                 />
               </div>
 
-              <div className="relative w-1/3">
+              <div className='relative w-1/3'>
                 <Image
-                  src="/logos/Visa_Logo_2014.svg"
-                  alt="paypal"
-                  loading="lazy"
+                  src='/logos/Visa_Logo_2014.svg'
+                  alt='paypal'
+                  loading='lazy'
                   fill
                 />
               </div>
@@ -59,40 +59,40 @@ export default function Footer() {
           </div>
         </div>
 
-        <div className="py-8 md:py-16 grid gap-8 sm:grid-cols-2 w-full border-t-2 border-gray-100">
-          <div className=" text-xl sm:text-2xl lg:text-3xl bg-blue-3">
-            <h3 className="footer-header ">Get in touch</h3>
-            <ul className="footer-list">
-              <li className="footer-li flex items-center gap-4 ">
+        <div className='py-8 md:py-16 grid gap-8 sm:grid-cols-2 w-full border-t-2 border-gray-100'>
+          <div className=' text-xl sm:text-2xl lg:text-3xl bg-blue-3'>
+            <h3 className='footer-header '>Get in touch</h3>
+            <ul className='footer-list'>
+              <li className='footer-li flex items-center gap-4 '>
                 <HiOutlinePhone size={16} />
                 <p>+63 9xx xxx xxxx</p>
               </li>
-              <li className="footer-li flex items-center gap-4">
+              <li className='footer-li flex items-center gap-4'>
                 <HiOutlineMail size={16} />
                 <p>freshta.business@gmail.com</p>
               </li>
             </ul>
           </div>
 
-          <div className="text-xl sm:text-2xl lg:text-3xl bg-red-3">
-            <h3 className="footer-header">Follow us on</h3>
-            <ul className="text-2xl sm:text-3xl lg:text-4xl mt-4 md:mt-6 lg:mt-8 flex gap-4 text-greeny-700 pb-4">
-              <li className="footer-follow-li">
-                <BsFacebook className="icon" />
+          <div className='text-xl sm:text-2xl lg:text-3xl bg-red-3'>
+            <h3 className='footer-header'>Follow us on</h3>
+            <ul className='text-2xl sm:text-3xl lg:text-4xl mt-4 md:mt-6 lg:mt-8 flex gap-4 text-greeny-700 pb-4'>
+              <li className='footer-follow-li'>
+                <BsFacebook className='icon' />
               </li>
-              <li className="footer-follow-li">
-                <BsTwitter className="icon" />
+              <li className='footer-follow-li'>
+                <BsTwitter className='icon' />
               </li>
-              <li className="footer-follow-li">
-                <BsInstagram className="icon" />
+              <li className='footer-follow-li'>
+                <BsInstagram className='icon' />
               </li>
             </ul>
           </div>
         </div>
 
-        <div className="mt-6 md:mt-12">
-          <p className="text-gray-500 text-lg sm:text-xl lg:text-2xl">
-            &copy; 2022 Freshta by <Link href="#">Alrho</Link>. All rights
+        <div className='mt-6 md:mt-12'>
+          <p className='text-gray-500 text-lg sm:text-xl lg:text-2xl'>
+            &copy; 2022 Freshta by <Link href='#'>Alrho</Link>. All rights
             reserved.
           </p>
         </div>
--- components/Users/LoginForm.tsx
+++ components/Users/LoginForm.tsx
@@ -1,22 +1,22 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import Option from "./Option";
-import { CgSpinner } from "react-icons/cg";
-import { loginUser } from "@/utils/helpers/loginUser";
-import { useRouter, useSearchParams } from "next/navigation";
-import InputField from "./InputField";
-import { useSession } from "next-auth/react";
+'use client';
+import React, { useState, useEffect } from 'react';
+import Option from './Option';
+import { CgSpinner } from 'react-icons/cg';
+import { loginUser } from '@/utils/helpers/loginUser';
+import { useRouter, useSearchParams } from 'next/navigation';
+import InputField from './InputField';
+import { useSession } from 'next-auth/react';
 
 const loginFormInit = {
-  email: "",
-  password: "",
+  email: '',
+  password: '',
 };
 
 export default function LoginForm() {
-  const [focusedInput, setFocusedInput] = useState("");
+  const [focusedInput, setFocusedInput] = useState('');
   const [formData, setFormData] = useState(loginFormInit);
   const [isLoading, setIsLoading] = useState(false);
-  const [submitError, setSubmitError] = useState<string>("");
+  const [submitError, setSubmitError] = useState<string>('');
   const { data: session } = useSession({
     required: false,
   });
@@ -30,7 +30,7 @@ export default function LoginForm() {
   }
 
   function handleBlur() {
-    setFocusedInput("");
+    setFocusedInput('');
   }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -45,15 +45,15 @@ export default function LoginForm() {
   async function handleLogin(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setIsLoading(true);
-    setSubmitError("");
+    setSubmitError('');
 
     try {
       const response = await loginUser(formData);
 
       if (response && response.error !== null) {
-        setSubmitError(response.error || "");
+        setSubmitError(response.error || '');
       } else {
-        router.push("/"); // TODO modify this to depend on URL search params
+        router.push('/'); // TODO modify this to depend on URL search params
       }
     } catch (err) {
       if (err instanceof Error) {
@@ -65,14 +65,14 @@ export default function LoginForm() {
   }
 
   return (
-    <div className="mx-8 my-36 font-poppins">
-      <h2 className="text-4xl font-semibold text-greeny-600 my-12">Login</h2>
+    <div className='mx-8 mt-36 mb-48 font-poppins sm:max-w-3xl sm:mx-auto'>
+      <h2 className='text-4xl font-semibold text-greeny-600 my-12'>Login</h2>
       <div>
-        <form className="flex flex-col gap-8" onSubmit={handleLogin}>
+        <form className='flex flex-col gap-8' onSubmit={handleLogin}>
           <InputField
-            type="email"
-            name="email"
-            label="Email address"
+            type='email'
+            name='email'
+            label='Email address'
             value={formData.email}
             onChange={handleInputChange}
             onFocus={handleFocus}
@@ -81,9 +81,9 @@ export default function LoginForm() {
           />
 
           <InputField
-            type="password"
-            name="password"
-            label="Password"
+            type='password'
+            name='password'
+            label='Password'
             value={formData.password}
             onChange={handleInputChange}
             onFocus={handleFocus}
@@ -92,26 +92,26 @@ export default function LoginForm() {
           />
 
           {isLoading && (
-            <CgSpinner className="animate-spin self-center text-6xl text-gray-700" />
+            <CgSpinner className='animate-spin self-center text-6xl text-gray-700' />
           )}
 
           {submitError && (
-            <p className="text-red-500 text-center text-lg font-semibold">
+            <p className='text-red-500 text-center text-lg font-semibold'>
               {submitError}
             </p>
           )}
 
           <button
-            type="submit"
+            type='submit'
             className={`bg-greeny-400 py-4 text-2xl text-white mt-4 rounded-xl ${
-              isLoading && "opacity-50"
+              isLoading && 'opacity-50'
             }`}
             disabled={isLoading}
           >
             Sign In
           </button>
         </form>
-        <Option to={"/account/register"} />
+        <Option to={'/account/register'} />
       </div>
     </div>
   );
